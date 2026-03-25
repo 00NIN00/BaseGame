b8c10b7a5f2e1a3452c25335a4d7018ec9ddc317
0NIN0 <126891638+00NIN00@users.noreply.github.com>
2026-03-25 19:29:44 +0300

    Start Save System Created PlayerData

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagement/PlayerData.cs
@@ -0,0 +1,10 @@
+using _Project.Develop.Runtime.Meta.Features.Wallet;
+using System.Collections.Generic;
+
+namespace _Project.Develop.Runtime.Utilities.DataManagement
+{
+    public class PlayerData
+    {
+        public Dictionary<CurrencyType, int> WalletData;
+    }
+}
