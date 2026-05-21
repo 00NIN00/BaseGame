73f795fd33fe267f08c0bef9986a84acb3f13ec8
0NIN0 <126891638+00NIN00@users.noreply.github.com>
2026-05-21 15:53:59 +0300

    Create OpenResetPopup

--- Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuPopupService.cs
+++ Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuPopupService.cs
@@ -1,4 +1,5 @@
 using _Project.Develop.Runtime.UI.Core;
+using _Project.Develop.Runtime.UI.ResetPopup;
 using UnityEngine;
 
 namespace _Project.Develop.Runtime.UI.MainMenu
@@ -20,5 +21,16 @@ namespace _Project.Develop.Runtime.UI.MainMenu
         }
 
         protected override Transform PopupLayer => _uiRoot.PopupsLayer;
+        
+        public ResetPopupPresenter OpenResetPopup()
+        {
+            ResetPopupView view = ViewsFactory.Create<ResetPopupView>(ViewIDs.ResetPopupView, PopupLayer);
+             
+            ResetPopupPresenter popup = _mainMenuPresentsFactory.CreateResetPopupPresenter(view);
+             
+            OnPopupCreated(popup, view);
+            
+            return popup;
+        }
     }
 }
