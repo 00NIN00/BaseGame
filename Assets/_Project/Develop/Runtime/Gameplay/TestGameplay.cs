9ea023e6825800df7cc924bf84f075c15f60c9e9
0NIN0 <126891638+00NIN00@users.noreply.github.com>
2026-06-29 13:19:42 +0300

    create Ghost

--- Assets/_Project/Develop/Runtime/Gameplay/TestGameplay.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/TestGameplay.cs
@@ -21,7 +21,8 @@ namespace _Project.Develop.Runtime.Gameplay
 
         public void Run()
         {
-            _entity = _entitiesFactory.CreateTestPlayerEntity(Vector3.zero);
+            _entity = _entitiesFactory.CreateGhost(Vector3.zero);
+            // _entity = _entitiesFactory.CreateTestPlayerEntity(Vector3.zero);
             
             _isRunning = true;
         }
@@ -30,6 +31,12 @@ namespace _Project.Develop.Runtime.Gameplay
         {
             if (_isRunning== false)
                 return;
+
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+            {
+                _entity.CurrentHealth.Value -= 50;
+                Debug.Log($"Current Health: {_entity.CurrentHealth.Value}");
+            }
             
             Vector3 inputDirection = new Vector3(UnityEngine.Input.GetAxisRaw("Horizontal"), 0, UnityEngine.Input.GetAxisRaw("Vertical"));
             
